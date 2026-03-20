383d4ab8d0ad246fd25c9ccca3a1a5f857310b34
yogi99k <yogikoteru99@gmail.com>
2026-03-20 00:36:56 -0400

    Users Mapper, Dto

---
+++ src/main/java/com/ecommerce/backend/mapper/UsersMapper.java
@@ -0,0 +1,40 @@
+package com.ecommerce.backend.mapper;
+
+import com.ecommerce.backend.dto.UsersDto;
+import com.ecommerce.backend.entity.Users;
+
+public class UsersMapper {
+    //the method to convert Users entity to UsersDto, i.e. to convert the data from the database
+    // to the format that we want to return to the client
+    public static UsersDto toDto(Users users){
+
+        UsersDto usersDto = new UsersDto(
+//                users.getUser_id(),
+//                users.getName(),
+//                users.getEmail(),
+//                users.getCity(),
+//                users.getSignup_date()
+        );
+        usersDto.setUser_id(users.getUser_id());
+        usersDto.setName(users.getName());
+        usersDto.setEmail(users.getEmail());
+        usersDto.setCity(users.getCity());
+        usersDto.setSignup_date(users.getSignup_date());
+
+        return usersDto;
+    }
+
+    //the method to convert UsersDto to Users entity,
+    // i.e. to convert the data from the format that we receive from the client
+    public static Users toEntity(UsersDto usersDto){
+
+        Users users = new Users();
+        users.setUser_id(usersDto.getUser_id());
+        users.setName(usersDto.getName());
+        users.setEmail(usersDto.getEmail());
+        users.setCity(usersDto.getCity());
+        users.setSignup_date(usersDto.getSignup_date());
+
+        return users;
+    }
+}
