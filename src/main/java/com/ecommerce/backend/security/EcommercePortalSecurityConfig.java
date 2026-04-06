ebae6fa299e3c5cf4ca753f32540e47401803171
yogi99k <yogikoteru99@gmail.com>
2026-04-06 14:29:24 -0400

    Spring Security Login

--- src/main/java/com/ecommerce/backend/security/EcommercePortalSecurityConfig.java
+++ src/main/java/com/ecommerce/backend/security/EcommercePortalSecurityConfig.java
@@ -6,6 +6,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.RegexRequestMatcher;
 
@@ -45,5 +51,19 @@ public class EcommercePortalSecurityConfig {
                 .formLogin(withDefaults())
                 .httpBasic(withDefaults())
                 .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        var user1 = User.builder().username("Yogi").password(passwordEncoder().encode("Yogi123#@"))
+                .roles("USER").build();
+        var admin = User.builder().username("admin").password(passwordEncoder().encode("Admin123#@"))
+                .roles("ADMIN").build();
+        return new InMemoryUserDetailsManager(user1, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
